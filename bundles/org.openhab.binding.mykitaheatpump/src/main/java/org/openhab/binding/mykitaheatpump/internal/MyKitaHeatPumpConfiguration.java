d71b27b2949003b0de3cc3133ba575e9658dd3e3
Marco Tombesi <32168423+baggior@users.noreply.github.com>
2019-07-24 02:08:46 +0200

    pollers

--- bundles/org.openhab.binding.mykitaheatpump/src/main/java/org/openhab/binding/mykitaheatpump/internal/MyKitaHeatPumpConfiguration.java
+++ bundles/org.openhab.binding.mykitaheatpump/src/main/java/org/openhab/binding/mykitaheatpump/internal/MyKitaHeatPumpConfiguration.java
@@ -10,7 +10,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.openhab.binding.mykitaheatpump;
+package org.openhab.binding.mykitaheatpump.internal;
 
 /**
  * The {@link MyKitaHeatPumpConfiguration} class contains fields mapping thing configuration parameters.
