79f7a812a716dc1eecc46c6a6beb7fe55b5c6c05
marco tombesi <marco.tombesi@gmail.com>
2019-09-05 13:57:16 +0200

    adding real channels

--- bundles/org.openhab.binding.mykitaheatpump/src/main/java/org/openhab/binding/mykitaheatpump/internal/ChannelsBuilder.java
+++ bundles/org.openhab.binding.mykitaheatpump/src/main/java/org/openhab/binding/mykitaheatpump/internal/ChannelsBuilder.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.smarthome.core.thing.Channel;
 import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.binding.builder.ChannelBuilder;
 import org.eclipse.smarthome.core.thing.type.ChannelKind;
+import org.eclipse.smarthome.core.thing.type.ChannelTypeUID;
 import org.openhab.binding.mykitaheatpump.internal.models.KitaHeatPump;
 import org.openhab.binding.mykitaheatpump.internal.models.KitaHeatPumpDataType.DataTypeEnum;
 
@@ -33,21 +35,71 @@ public class ChannelsBuilder {
 
         this.kita.getData().forEach((dataType, dataValue) -> {
             // todo
-            String label = dataType.name;
-            String description = dataType.description;
+            String id = dataType.name;
+            String label = dataType.label;
 
-            ChannelUID channelUID = channelsHandler.getChannelUID(label);
-            // ChannelUID channelUID = new ChannelUID(thingUid + ":" + label);
-
+            ChannelUID channelUID = channelsHandler.getChannelUID(id);
             String acceptedItemType = this.convertToItemType(dataType.type);
 
-            Channel ch = ChannelBuilder.create(channelUID, acceptedItemType).withLabel(label)
-                    .withKind(ChannelKind.STATE).withDescription(description).build();
+            ChannelBuilder builder = ChannelBuilder.create(channelUID, acceptedItemType).withLabel(label);
 
+            ChannelTypeUID ctypeUUID = this.convertToChannelType(dataType.type);
+            if (ctypeUUID != null) {
+                builder.withType(ctypeUUID);
+            }
+
+            ChannelKind cKind = this.convertToChannelKind(dataType.type);
+            if (cKind != null) {
+                builder.withKind(cKind);
+            }
+
+            Channel ch = builder.build();
             ret.add(ch);
         });
 
         return ret;
+    }
+
+    private @Nullable ChannelKind convertToChannelKind(DataTypeEnum type) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    private @Nullable ChannelTypeUID convertToChannelType(DataTypeEnum type) {
+        // TODO Auto-generated method stub
+        ChannelTypeUID ctypeUUID = null;
+        switch (type) {
+            case _bool:
+                break;
+            case _switch:
+                break;
+            case dateTime:
+                break;
+            case number:
+                break;
+            case string:
+                break;
+            case temperature_ro:
+                ctypeUUID = channelsHandler.getChannelTypeUID("temperature-ro-ctype");
+                break;
+            case temperature_rw:
+                ctypeUUID = channelsHandler.getChannelTypeUID("temperature-rw-ctype");
+                break;
+            case flow_ro:
+                ctypeUUID = channelsHandler.getChannelTypeUID("flow-ro-ctype");
+                break;
+            case pct:
+                ctypeUUID = channelsHandler.getChannelTypeUID("pct-ctype");
+                break;
+            case cop:
+                ctypeUUID = channelsHandler.getChannelTypeUID("cop-ctype");
+                break;
+            default:
+                break;
+
+        }
+
+        return ctypeUUID;
     }
 
     private String convertToItemType(DataTypeEnum type) {
@@ -57,15 +109,21 @@ public class ChannelsBuilder {
 
             case number:
                 return "Number";
+            case temperature_ro:
+            case temperature_rw:
+                return "Number:Temperature";
+            case pct:
+            case cop:
+                return "Number:Dimensionless";
+            case flow_ro:
+                return "Number:VolumetricFlowRate";
 
-            case string:
-                return "String";
-
+            case _switch:
             case _bool:
                 return "Switch";
 
-            case _switch:
-                return "Switch";
+            case string:
+                return "String";
 
             default:
                 throw new RuntimeException("DataTypeEnum unknown");
